424e4467824876b0019a0c0e4f493a69d391fd31
Lennart <schmidt_lennart@mail.de>
2026-08-04 19:44:05 +0000

    Add migration script to merge icon generation and agent confirmation heads

---
+++ backend/migrations/versions/087b914c9e72_merge_icon_generation_and_agent_.py
@@ -0,0 +1,24 @@
+"""merge icon generation and agent confirmation heads
+
+Revision ID: 087b914c9e72
+Revises: 1234567890ab, 1a7d9e3c5b20
+Create Date: 2026-08-04 19:42:48.373103
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '087b914c9e72'
+down_revision = ('1234567890ab', '1a7d9e3c5b20')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
